3b332f34c02005de534ef1ec153eaef778966a09
Imobach Martín <martin@365farmnet.com>
2023-12-02 14:42:03 +0000

    Green

--- server/services/game.service.js
+++ server/services/game.service.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+class Game {
+  get state() {
+    return {
+      id: this.id,
+    };
+  }
+  constructor() {
+    this.id = 'someId';
+  }
+}
+
 // Game api route.
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+router.post('/', (req, res) => {
+  const newGame = new Game();
+  res.json(newGame.state);
 });
 
 module.exports = router;
