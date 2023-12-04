343a5d6d2c2e17f16b2d416f7db32736224db4ad
Imobach Martín <martin@365farmnet.com>
2023-12-04 15:23:44 +0000

    Style client game

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       // setTimeout(async () =>{
       const response = await createGame();
-      setGameState(response);
+      setGameState(await response.json());
       setStarted(true);
       // },1000);
     } catch (ex) {
@@ -29,13 +29,27 @@ function App() {
         <h1>BlackJack</h1>
       </header>
       <main className="App-main">
+        <img src={dealerLogo} className="dealer-logo" alt="Dealer" />
         <section className="App-dealer">
-          <img src={dealerLogo} className="dealer-logo" alt="Dealer" />
+          <article className="points">&nbsp;</article>
           <div className="dealer-card-list">
+            {gameState?.dealer?.cards.map((card, index) => {
+              if (index === 0) {
+               return <>
+                <article key={card.face + card.suit} className="card">{card.face} {card.suit}</article>
+                <article className="card card-hidden"></article>
+               </>
+              }
+            })}
           </div>
         </section>
         <section className="App-player">
-          <div className="player-card-list"></div>
+          <article>{gameState?.players?.[0].points ? gameState?.players?.[0].points + " Points" : ""}</article>
+          <div className="player-card-list">
+            {gameState?.players?.[0].cards.map((card) => (
+              <article key={card.face + card.suit} className="card">{card.face} {card.suit}</article>
+            ))}
+          </div>
         </section>
         <section className="App-action-list">
           {started ? (
