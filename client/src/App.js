1307f4fe5530678b2d2da4176b606fbfbdab079e
Imobach Martín <martin@365farmnet.com>
2023-12-04 13:42:44 +0000

    Update client game template

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import dealerLogo from './dealer.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BlackJack</h1>
       </header>
+      <main className="App-main">
+        <section className="App-dealer">
+          <img src={dealerLogo} className="dealer-logo" alt="Dealer" />
+          <div className="dealer-card-list">
+          </div>
+        </section>
+        <section className="App-player">
+          <div className="player-card-list">
+          </div>
+        </section>
+        <section className="App-action-list">
+          <div className="App-action">
+            <button>New Game</button>
+          </div>
+          <div className="App-action">
+            <button>Hit</button>
+          </div>
+          <div className="App-action">
+            <button>Hold</button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
