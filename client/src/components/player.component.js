d7c80ce703672b463747495642b10d7f3206a9a3
Imobach Martín <martin@365farmnet.com>
2023-12-05 16:53:04 +0000

    Green

--- client/src/components/player.component.js
+++ client/src/components/player.component.js
@@ -4,8 +4,8 @@ export const Player = ({playerCards, playerPoints}) => {
     <label tabIndex={0} aria-label={`Player points: ${playerPoints ? playerPoints + " Points" : ""}`}>{playerPoints ? playerPoints + " Points" : ""}</label>
     { playerCards.length > 0 ?
     <div className="player-card-list">
-      {playerCards.map((card) => (
-        <figure key={card.face + card.suit} className="card">
+      {playerCards.map((card,i) => (
+        <figure role="figure" key={card.face + card.suit} className="card" data-testid={`player-card-${i + 1}`}>
           <div className="card-image" data-face={card.face} data-suit={card.suit}></div>
           <figcaption tabIndex={0} className="hidden-accessible" aria-label={`Player card: ${card.face} ${card.suit}`}></figcaption>
         </figure>
