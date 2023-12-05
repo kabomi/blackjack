3ca2dac70e350416db4d737e3cb184fa1347bce7
Imobach Martín <martin@365farmnet.com>
2023-12-05 22:15:04 +0000

    Add Server code coverage by running yarn test:ci

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,24 @@ const config = {
   testEnvironment: 'node',
   verbose: true,
   modulePathIgnorePatterns: ['<rootDir>/client/'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+  coverageDirectory: '<rootDir>/server/coverage',
+  coverageReporters: ['text', 'html', 'cobertura'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!client/**/*',
+    '!coverage/**/*',
+    '!jest.config.js',
+  ],
+  resetMocks: true,
+  restoreMocks: true,
 };
 
 module.exports = config;
