40393301406495493d563c0574110b75c17c3c71
Imobach Martín <martin@365farmnet.com>
2023-12-05 17:30:06 +0000

    Refactor. Remove duplication

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -35,80 +35,85 @@ describe('App', () => {
     expect(buttonElement.textContent).toBe('New Game');
   });
   describe('On New Game', () => {
+    let gameState;
     beforeEach(() => {
+      gameState = {
+        id: 'harry',
+        dealer: {
+          cards: [{ face: 'A', suit: 'Spades'}, { face: '9', suit: 'Spades'}]
+        },
+        players: [{
+          cards: [{ face: 'A', suit: 'Clubs'}, { face: '10', suit: 'Clubs'}],
+          points: 18,
+        }]
+      };
       fetchSpy.mockResolvedValue({
-        json: async () => ({
-          id: 'harry',
-          dealer: {
-            cards: [{ face: 'A', suit: 'Spades'}, { face: '9', suit: 'Spades'}]
-          },
-          players: [{
-            cards: [{ face: 'A', suit: 'Spades'}, { face: '9', suit: 'Spades'}],
-            points: 18,
-          }]
-        })
+        json: async () => (gameState)
       });
     });
     it('should render the dealers hand', async () => {
       render(<App />);
+      
 
-      const newGameElement = await within(screen.getByTestId('action-list')).findByRole("button");
+      const newGameElement = within(screen.getByTestId('action-list')).getByRole("button");
       await userEvent.click(newGameElement);
       
       // Cards
-      const firstCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-1");
-      const secondCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-2");
+      const firstCard = screen.getByTestId('dealer-card-1');
+      const secondCard = screen.getByTestId('dealer-card-2');
       expect(firstCard).toBeDefined();
       expect(secondCard).toBeDefined();
       // Points
-      const points = await within(screen.getByTestId('dealer')).findByTestId("dealer-points");
+      const points = screen.getByTestId('dealer-points');
       expect(points.textContent).toMatch(/\s/g);
     });
     it('should render the dealer card images with the right face and suit attributes', async () => {
+      const dealerCards = gameState.dealer.cards;
       render(<App />);
 
-      const newGameElement = await within(screen.getByTestId('action-list')).findByRole("button");
+      const newGameElement = within(screen.getByTestId('action-list')).getByRole("button");
       await userEvent.click(newGameElement);
       
       // Card Images
-      const firstCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-1");
-      const secondCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-2");
-      const firstCardImage = await within(firstCard).findByRole("img");
-      expect(firstCardImage.getAttribute('data-face')).toEqual('A');
-      expect(firstCardImage.getAttribute('data-suit')).toEqual('Spades');
+      const firstCard = screen.getByTestId('dealer-card-1');
+      const secondCard = screen.getByTestId('dealer-card-2');
+      const firstCardImage = within(firstCard).getByRole("img");
+      expect(firstCardImage.getAttribute('data-face')).toEqual(dealerCards[0].face);
+      expect(firstCardImage.getAttribute('data-suit')).toEqual(dealerCards[0].suit);
       expect(secondCard.getAttribute('data-face')).toBeFalsy();
       expect(secondCard.getAttribute('data-suit')).toBeFalsy();
     });
     it('should render the player\'s hand', async () => {
       render(<App />);
 
-      const newGameElement = await within(screen.getByTestId('action-list')).findByRole("button");
+      const newGameElement = within(screen.getByTestId('action-list')).getByRole("button");
       await userEvent.click(newGameElement);
 
       // Cards
-      const firstCard = await within(screen.getByTestId('player')).findByTestId("player-card-1");
-      const secondCard = await within(screen.getByTestId('player')).findByTestId("player-card-2");
+      const firstCard = screen.getByTestId('player-card-1');
+      const secondCard = screen.getByTestId('player-card-2');
       expect(firstCard).toBeDefined();
       expect(secondCard).toBeDefined();
       // Points
-      const points = await within(screen.getByTestId('player')).findByTestId("player-points");
+      const points = screen.getByTestId('player-points');
       expect(points.textContent).toMatch(/\d Points/g);
     });
     it('should render the player card images with the right face and suit attributes', async () => {
+      const playerCards = gameState.players[0].cards;
       render(<App />);
 
-      const newGameElement = await within(screen.getByTestId('action-list')).findByRole("button");
+      const newGameElement = within(screen.getByTestId('action-list')).getByRole("button");
       await userEvent.click(newGameElement);
       
       // Card Images
-      const firstCard = await within(screen.getByTestId('player')).findByTestId("player-card-1");
-      const secondCard = await within(screen.getByTestId('player')).findByTestId("player-card-2");
-      const firstCardImage = await within(firstCard).findByRole("img");
-      const secondCardImage = await within(secondCard).findByRole("img");
-      expect(firstCardImage.getAttribute('data-face')).toEqual('A');
-      expect(firstCardImage.getAttribute('data-suit')).toEqual('Clubs');
-      expect(secondCardImage.getAttribute('data-face')).toEqual('10');
-      expect(secondCardImage.getAttribute('data-suit')).toEqual('Clubs');
+      const firstCard = screen.getByTestId('player-card-1');
+      const secondCard = screen.getByTestId('player-card-2');
+      const firstCardImage = within(firstCard).getByRole("img");
+      const secondCardImage = within(secondCard).getByRole("img");
+      expect(firstCardImage.getAttribute('data-face')).toEqual(playerCards[0].face);
+      expect(firstCardImage.getAttribute('data-suit')).toEqual(playerCards[0].suit);
+      expect(secondCardImage.getAttribute('data-face')).toEqual(playerCards[1].face);
+      expect(secondCardImage.getAttribute('data-suit')).toEqual(playerCards[1].suit);
     });
   });
 });
