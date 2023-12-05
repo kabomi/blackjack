18593d169cd680d8b7e65ef8db87b1bb4b6fe68f
Imobach Martín <martin@365farmnet.com>
2023-12-05 16:47:43 +0000

    Red. On New Game. should render the dealers hand

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,17 @@
 import { render, screen, within } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
+  /** @type {jest.SpyInstance<Promise<Response>} */
+  let fetchSpy;
+  beforeEach(() => {
+    fetchSpy = jest.spyOn(window, 'fetch');
+    jest.resetAllMocks();
+  });
+  afterAll(() => {
+    jest.resetAllMocks();
+  })
   it('renders the header, the Dealer, the Player and the ActionList', () => {
     render(<App />);
     const headerElement = screen.getByRole('heading');
@@ -19,7 +29,29 @@ describe('App', () => {
   it('should render only the new game action', async () => {
     render(<App />);
 
+    
+
     const buttonElement = await within(screen.getByTestId('action-list')).findByRole("button");
     expect(buttonElement.textContent).toBe('New Game');
+  });
+  describe('On New Game', () => {
+    it('should render the dealers hand', async () => {
+      render(<App />);
+
+      fetchSpy.mockResolvedValue({
+        json: async () => ({
+          id: 'harry',
+          dealer: {
+            cards: [{ face: 'A', suit: 'Spades'}, { face: '9', suit: 'Spades'}]
+          }
+        })
+      });
+      const newGameElement = await within(screen.getByTestId('action-list')).findByRole("button");
+      await userEvent.click(newGameElement);
+      const dealerFirstCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-1");
+      const dealerSecondCard = await within(screen.getByTestId('dealer')).findByTestId("dealer-card-2");
+      expect(dealerFirstCard).toBeDefined();
+      expect(dealerSecondCard).toBeDefined();
+    });
   });
 });
