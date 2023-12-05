7c6ce416539bfcebc029430ce4fd8857a5d29093
Imobach Martín <martin@365farmnet.com>
2023-12-05 17:53:27 +0000

    Green. ActionList should render other game actions than "new game"

--- client/src/components/ActionList.component.spec.js
+++ client/src/components/ActionList.component.spec.js
@@ -8,4 +8,11 @@ describe('ActionList', () => {
     const buttonElement = within(screen.getByTestId('action-list')).getByRole("button");
     expect(buttonElement.textContent).toBe('New Game');
   });
+  it('should render other game actions than "new game"', () => {
+    render(<ActionList showGameActions={true} />);
+
+    const buttonList = within(screen.getByTestId('action-list')).getAllByRole("button");
+    expect(buttonList[0].textContent).toBe('Hit');
+    expect(buttonList[1].textContent).toBe('Hold');
+  });
 });
