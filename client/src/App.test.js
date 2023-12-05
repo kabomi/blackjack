2a1a3aa9e79690d5b0b088ae561b03fefb5d2f4b
Imobach Martín <martin@365farmnet.com>
2023-12-05 15:52:22 +0000

    Red. App renders the header, the Dealer, the Player and the ActionList

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders the header, the Dealer, the Player and the ActionList', () => {
+    render(<App />);
+    const headerElement = screen.getByRole('heading');
+    expect(headerElement.textContent).toContain('BlackJack');
+  
+    const dealerElement = screen.getByTestId('dealer');
+    expect(dealerElement.textContent).toBeDefined();
+  
+    const playerElement = screen.getByTestId('player');
+    expect(playerElement.textContent).toBeDefined();
+  
+    const actionListElement = screen.getByTestId('action-list');
+    expect(actionListElement.textContent).toBeDefined();
+    
+  });
 });
