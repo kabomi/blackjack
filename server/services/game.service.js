9c564bdd035a36c994a35db6c4c2248ad6fb15d7
Imobach Martín <martin@365farmnet.com>
2023-12-06 18:53:49 +0000

    Green

--- server/services/game.service.js
+++ server/services/game.service.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const Game = require('../models/game.class');
 const dbConnection = require('../persistance/dbConnection');
 
-// Game api route.
 router.post('/', async (req, res) => {
   const newGame = Game.create();
   const dbClient = dbConnection.get();
   await dbClient.create(newGame.state);
+  res.json(newGame.state);
+});
+
+router.patch('/:id/hit', async (req, res) => {
+  const newGame = Game.create();
+  // const dbClient = dbConnection.get();
+  // await dbClient.create(newGame.state);
   res.json(newGame.state);
 });
 
