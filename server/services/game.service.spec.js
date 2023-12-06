e7561973ac2656f595614bfffd5c9de0b54dff95
Imobach Martín <martin@365farmnet.com>
2023-12-06 16:21:45 +0000

    Red. Game service should persist a new game on POST to /

--- server/services/game.service.spec.js
+++ server/services/game.service.spec.js
@@ -1,12 +1,27 @@
+const Game = require('../models/game.class.js');
+const dbConnection = require('../persistance/dbConnection');
+
+jest.mock('../persistance/dbConnection', () => {
+  return {
+    initialize: jest.fn().mockResolvedValue({ create: jest.fn() }),
+  };
+});
+
 describe('Game service', () => {
-  /** @type{import('supertest').SuperTest<supertest.Test>} */
+  /** @type {import('supertest').SuperTest<supertest.Test>} */
   let requestWithSupertest;
+  /** @type {import('../persistance/dbConnection').RxDbClient} */
+  let dbClient;
   beforeAll(() => {
     const server = require('../index.js');
     const supertest = require('supertest');
     requestWithSupertest = supertest(server);
   });
-  it('should generate a new game on POST', async () => {
+  beforeEach(async () => {
+    dbClient = await dbConnection.initialize();
+    jest.spyOn(dbClient, 'create').mockImplementation(() => jest.fn());
+  });
+  it('should generate a new game on POST to /', async () => {
     /** @type{import('express').Response} */
     const response = await requestWithSupertest.post('/api/game/');
     expect(response.status).toBe(200);
@@ -14,5 +29,11 @@ describe('Game service', () => {
     expect(response.body).toEqual(
       expect.objectContaining({ id: expect.any(String) })
     );
+  });
+  it('should persist a new game on POST to /', async () => {
+    const gameStub = { state: {} };
+    jest.spyOn(Game, 'create').mockImplementation(() => gameStub);
+    await requestWithSupertest.post('/api/game/');
+    expect(dbClient.create).toHaveBeenCalledWith(gameStub.state);
   });
 });
