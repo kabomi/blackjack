8052f2e590fd7b684659a73b3ce7e287d96d9d48
Imobach Martín <martin@365farmnet.com>
2023-12-06 21:50:13 +0000

    Red. RxDbClient should create a document

---
+++ server/persistance/rxdb/rxDbClient.spec.js
@@ -0,0 +1,19 @@
+const RxDbClient = require('./RxDbClient');
+jest.mock('../dbConnection', () => {
+  return {
+    game: {
+      insert: jest.fn(),
+    },
+  };
+});
+describe('RxDbClient', () => {
+  it('should create a document', () => {
+    const dbConnection = require('../dbConnection');
+    const client = new RxDbClient(dbConnection);
+    const gameState = {};
+
+    client.create('game', gameState);
+
+    expect(dbConnection.game.insert).toHaveBeenCalledWith(gameState);
+  });
+});
