40e5d597bce8dd49eb4e22258c1f48e79d13f212
Imobach Martín <martin@365farmnet.com>
2023-12-07 12:17:54 +0000

    Add lint script

--- server/persistance/dbClient.js
+++ server/persistance/dbClient.js
@@ -4,11 +4,11 @@ class DbClient {
     this._dbConnection = _dbConnection;
   }
   // eslint-disable-next-line no-unused-vars
-  async create(state) { }
+  async create(state) {}
   // eslint-disable-next-line no-unused-vars
-  async findById(state) { }
+  async findById(state) {}
   // eslint-disable-next-line no-unused-vars
-  async update(state) { }
+  async update(state) {}
 }
 
 module.exports = DbClient;
